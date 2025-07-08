6a3bc2fe48c0d8dad764f243b8e8665162c34b39
Filipe S <filipeschausst@gmail.com>
2025-07-08 13:37:42 -0300

    Adição de novas mecânicas

--- truco.c
+++ truco.c
@@ -15,6 +15,7 @@ void mostrar_baralho();
 int vira();
 void dar_cartas();
 void rodadas();
+void sort_carta(int *array, int size);
 
 typedef struct{
     char nome;
@@ -105,20 +106,19 @@ void mostrar_baralho(){
 }
 
 int vira(){
+
     int k = randomico(40);
     int j = ((k/4)*4) + 4;
 
-    if(j >= 40){
-        j = 0;
-    }
+    (j >= 40) && (j = 0);
 
     for(int i = 0; i < 4; i++, j++){
         baralho[j].valor = 10 + i;
     }
 
-    baralho[k].status = 1;
+    baralho[k].status = 2;
 
-    printf("A vira é %c %s\n\n", baralho[k].nome, baralho[k].naipe);
+    printf("A vira é: \n|\t %c %s \t|\n\n", baralho[k].nome, baralho[k].naipe);
 
     return k;
 }
@@ -143,11 +143,8 @@ void ler_players(){
     for(int i = 0; i < jogadores; i++){
 
         int opt;
-        if(i % 2 == 0){
-            payers[i].time = false;
-        }else{
-            payers[i].time = true;
-        }
+
+        (i % 2 == 0) ? (payers[i].time = false) : (payers[i].time = true);
 
         printf("Player %d, digite seu nome: ", i+1);
         fgets(payers[i].nome, 50, stdin);
@@ -159,11 +156,7 @@ void ler_players(){
             getchar();
         }while(opt != 0 && opt != 1);
 
-        if(opt == 0){
-            payers[i].cpu = false;
-        }else{
-            payers[i].cpu = true;
-        }
+        (opt == 0) ? (payers[i].cpu = false) : (payers[i].cpu = true);
 
         printf("Player %s, você faz parte do time %s e %s controlado pelo computador\n\n", payers[i].nome, times[(int)payers[i].time].nome, s[payers[i].cpu]);
     }
@@ -186,7 +179,7 @@ void dar_cartas(){
 
 void rodadas(){
 
-    int trucos, rodada, parcial[2], askTruco, opt, trucado;
+    int opt;
 
     do{
         printf("\n");
@@ -194,57 +187,51 @@ void rodadas(){
         vira();
         dar_cartas();
 
-        rodada = 0;
-        trucos = 0;
-        parcial[0] = 1;
-        parcial[1] = 0;
-        askTruco = -1;
-        trucado = -1;
-
+        int rodada = 0, trucos = 0, parcial[] = {0,0};
+        int askTruco = -1, trucado = -1, ganhador = -1, fazendo = -1;
+        //int cartasJogadas[2] = {-2,-2};
+        
         do{
-            rodada++;
+            int cartasJogadas[2] = {-2,-2};
+
             for(int i = 0; i < jogadores; i++){
 
+                int k = 0;
                 printf("Vez do Player %s\nAs suas cartas são:\n",payers[i].nome);
                 for(int j = 0; j < MAO; j++){
-                    if(baralho[payers[i].carta_payer[j]].status == -1){
-                        printf("Carta %d: %c %s\n", j+1, baralho[payers[i].carta_payer[j]].nome, baralho[payers[i].carta_payer[j]].naipe);
-                    }else{
-                        printf("Carta %d: JÁ JOGADA\n",j);
-                    }
+
+                    (baralho[payers[i].carta_payer[j]].status == -1) && printf("Carta %d: %c %s\n", ++k, baralho[payers[i].carta_payer[j]].nome, baralho[payers[i].carta_payer[j]].naipe);
+
                 }
                 printf("\n");
 
                 if((int)payers[i].time == trucado){
-                    (trucos == 0) ? printf("Pediram truco, aceitar? (1 = sim, 0 = não): ") : printf("Pediram %d, aceitar? (1 = sim, 0 = não): ", (trucos+1)*3);
 
-                    if(payers[i].cpu){
-                        opt = (randomico(3)) % 2;
-                    }else{
-                        do{
-                            scanf("%d", &opt);
-                        }while(opt != 0 && opt != 1);
-                    }
+                    do{
+                        (trucos == 0) ? printf("Pediram truco, aceitar? (1 = sim, 0 = não): ") : printf("Pediram %d, aceitar? (1 = sim, 0 = não): ", (trucos+1)*3);
+
+                        (payers[i].cpu) ? opt = (randomico(3)) % 2 : scanf("%d", &opt);
+
+                    }while(opt != 0 && opt != 1);
 
                     if(opt == 1){
                         trucos++;
                         trucado = -1;
                     }else{
+                        ganhador = (int)!payers[i].time;
                         goto fimRodada;
                     }
                 }
 
                 if(askTruco != (int)payers[i].time){
 
-                    (trucos == 0) ? printf("Você quer pedir truco? (1 = sim, 0 = não): ") : printf("Você quer pedir %d? (1 = sim, 0 = não): ", (trucos+1)*3);
+                    do{
 
-                    if(payers[i].cpu){
-                        opt = (randomico(3)) % 2;
-                    }else{
-                        do{
-                            scanf("%d", &opt);
-                        }while(opt != 0 && opt != 1);
-                    }
+                        (trucos == 0) ? printf("Você quer pedir truco? (1 = sim, 0 = não): ") : printf("Você quer pedir %d? (1 = sim, 0 = não): ", (trucos+1)*3);
+
+                        (payers[i].cpu) ? opt = (randomico(3)) % 2 : scanf("%d", &opt);
+
+                    }while(opt != 0 && opt != 1);
 
                     if(opt == 1){
                         askTruco = (int)payers[i].time;
@@ -253,20 +240,75 @@ void rodadas(){
                     
                 }
 
-                printf("Que carta você quer jogar? (digite 1, 2 ou 3): ");
-                if(payers[i].cpu){
-                    opt = randomico(3) + 1;
-                }else{
-                    do{
-                        scanf("%d", &opt);
-                    }while(opt < 1 || opt > 3);
+                do{
+
+                    (k > 1) ? printf("Que carta você quer jogar? (digite um número entre 1 e %d): ",k) : printf("Digite 1 para jogar sua última carta: ");
+
+                    (payers[i].cpu) ? opt = randomico(k) + 1 : scanf("%d", &opt);
+
+                }while(opt < 1 || opt > k);
+
+                for(int cont = 0, temp = 0; cont < MAO; cont++){
+
+                    (baralho[payers[i].carta_payer[cont]].status == -1) && temp++;
+
+                    (temp == opt) && (opt = cont);
+
                 }
+
+                (payers[i].carta_payer[opt] > cartasJogadas[(int)payers[i].time]) && (cartasJogadas[(int)payers[i].time] = payers[i].carta_payer[opt]);
+
+                baralho[payers[i].carta_payer[opt]].status = 1;
+
+
+                if(baralho[cartasJogadas[0]].valor > baralho[cartasJogadas[1]].valor){
+                    printf("\nA carta | %c %s | do time \"%s\" está fazendo\n\n", baralho[cartasJogadas[0]].nome, baralho[cartasJogadas[0]].naipe, times[0].nome);
+                    fazendo = 0;
+                }else if(baralho[cartasJogadas[0]].valor < baralho[cartasJogadas[1]].valor){
+                    printf("\nA carta | %c %s | do time \"%s\" está fazendo\n\n", baralho[cartasJogadas[1]].nome, baralho[cartasJogadas[1]].naipe, times[1].nome);
+                    fazendo = 1;
+                }else{
+                    printf("Empachado, entre as cartas | %c %s | e | %c %s |\n\n", baralho[cartasJogadas[0]].nome, baralho[cartasJogadas[0]].naipe, baralho[cartasJogadas[1]].nome, baralho[cartasJogadas[1]].naipe);
+                    fazendo = -1;
+                }
+
             }
 
-            fimRodada:
+            if(fazendo == -1){
+                printf("A rodada terminou empachada\n\n");
+            }else{
+                printf("o time %s ganhou essa rodada\n\n", times[fazendo].nome);
+                parcial[fazendo]++;
+            }
 
-        }while(rodada < 2 || parcial[0] == parcial[1]);
+            rodada++;
+        }while((rodada < 2 || parcial[0] == parcial[1]) && rodada < 3);
+
+        fimRodada:
+
+        
 
     }while(times[0].pontos < 12 && times[1].pontos < 12);
+
+}
+
+void sort_carta(int *array, int size){
+
+    int buffer;
+
+    for(int i = 0; i < size; i++){
+        for(int j = i+1; j < size; j++){
+
+            if(*(array+i) != -2 && *(array+j) != -2){
+                if(baralho[*(array+i)].valor <= baralho[*(array+j)].valor){
+
+                    buffer = *(array+i);
+                    *(array+i) = *(array+j);
+                    *(array+j) = buffer;
+
+                }
+            }
+        }
+    }
 
 }
